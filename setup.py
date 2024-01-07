05ea98cc864792f4b32ecce43654082e28b4c404
Hynek Schlawack <hs@ox.cx>
2024-01-07 12:06:35 +0800

    alvistack/23.1.0
        git clean -xdf
        tar zcvf ../python-service-identity_23.1.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-service-identity.spec ../python-service-identity_23.1.0-1.spec
        cp ../python*-service-identity*23.1.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/pyca-service-identity-23.1.0/
        rm -rf ../python*-service-identity*23.1.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='service-identity',
+    version='23.1.0',
+    description='Service identity verification for pyOpenSSL & cryptography.',
+    author_email='Hynek Schlawack <hs@ox.cx>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+    ],
+    install_requires=[
+        'attrs>=19.1.0',
+        'cryptography',
+        'pyasn1',
+        'pyasn1-modules',
+    ],
+    extras_require={
+        'dev': [
+            'pyopenssl',
+            'service-identity[docs,idna,mypy,tests]',
+        ],
+        'docs': [
+            'furo',
+            'myst-parser',
+            'pyopenssl',
+            'sphinx',
+            'sphinx-notfound-page',
+        ],
+        'idna': [
+            'idna',
+        ],
+        'mypy': [
+            'idna',
+            'mypy',
+            'types-pyopenssl',
+        ],
+        'tests': [
+            'coverage[toml]>=5.0.2',
+            'pytest',
+        ],
+    },
+    packages=[
+        'service_identity',
+    ],
+    package_dir={'': 'src'},
+)
